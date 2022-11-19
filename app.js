eba41e51cd818fbf1e2913a926ab46cdf869eabf
Noe-Sanchez <danfoot4567@gmail.com>
2022-11-19 16:33:35 -0600

    Documentation

--- app.js
+++ app.js
@@ -1,101 +1,54 @@
+//Backend del Equipo 5 de TC1004B.508
+//
+//Hacemos los imports
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const fs = require('fs')
 const path = require('path')
 const morgan = require('morgan')
-const router = require('./routes/route')
-//const sqlscr = require('./sql_trial')
 const app = express()
+const config = require('./config/db.config.js')
 
 app.use(cors())
 
-// parse application/x-www-form-urlencoded
+//Traemos el parser
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
 app.use(bodyParser.json())
 
+//Traemos morgan para loggear
 app.use(morgan('dev'))
 
-// create a write stream (in append mode)
+//Creamos el stream para loggear
 var accessLogStream = fs.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' })
- 
-// setup the logger
 app.use(morgan('combined', { stream: accessLogStream }))
-app.use(router)
 
-//app.use(express.static(path.join(__dirname,'sql_trial.js')));
-//app.use(express.static('IoT'))
-app.use(express.static(__dirname))
-console.log(__dirname)
-
+//Ruta para la pagina principal
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
-  //res.sendFile(path.join(__dirname, 'sql_trial.js'))
 })
 
-app.get('/getData', function(req, res) {
-  res.status(200).json({info: 'preset text'})
-})
-
-app.get('/getData2', function(req, res) {
-
-var mysql      = require('mysql2');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'danfo',
-  password : 'D4062.tectec',
-  database : 'iot'
-});
-
-connection.connect();
-
-connection.query('SELECT * FROM sensores', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The query result is: ', results);
-  res.status(200).json({info: results[0].log_id});
-  //document.getElementById("lista").innerHTML = results;
-});
-
-
-//hdosjhfojf
-
-connection.end();
-})
-
+//Ruta para la lista de sensores
 app.get('/getSensorList', function(req, res) {
+  //Requerimos el modulo para sql
+  var mysql = require('mysql2');
 
-var mysql      = require('mysql2');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'danfo',
-  password : 'D4062.tectec',
-  database : 'iot'
-});
+  //Conectamos a la base de datos con la info de /config/db.config.js
+  var connection = mysql.createConnection({host: config.HOST, user: config.USER, password: config.PASSWORD, database: config.DB});
+  connection.connect();
 
-connection.connect();
+  //Hacemos la query
+  connection.query('SELECT nombre FROM sensores', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The query result is: ', results);
+    res.status(200).json({info: results});
+  });
 
-connection.query('SELECT nombre FROM sensores', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The query result is: ', results);
-  res.status(200).json({info: results});
-  //document.getElementById("lista").innerHTML = results;
-});
-
-
-//hdosjhfojf
-
-connection.end();
+  //Terminamos la conexion
+  connection.end();
 })
-
-
-
-
 
 const port = 3100
-
-// app.listen(process.env.PORT || port , (err) => {
 app.listen(port, () => {
   console.log('Server started running on : ' + port)
 })
