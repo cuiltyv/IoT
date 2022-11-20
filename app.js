41cfc9c57c4fab808ee70ddde6175c260b5a484e
Noe45 <79063530+Noe-Sanchez@users.noreply.github.com>
2022-11-19 22:41:12 -0600

    Fix merging traces

--- app.js
+++ app.js
@@ -1,53 +1,12 @@
-<<<<<<< HEAD
-=======
 //Backend del Equipo 5 de TC1004B.508
 //
 //Hacemos los imports
->>>>>>> feature/html_requests
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const fs = require('fs')
 const path = require('path')
 const morgan = require('morgan')
-<<<<<<< HEAD
-const router = require('./routes/route')
-const app = express()
-
-app.use(cors())
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-app.use(morgan('dev'))
-
-// create a write stream (in append mode)
-var accessLogStream = fs.createWriteStream(path.join(__dirname, '/logs/access.log'), { flags: 'a' })
- 
-// setup the logger
-app.use(morgan('combined', { stream: accessLogStream }))
-app.use(router)
-
-/*app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-*/
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-
-const port = 3100
-
-// app.listen(process.env.PORT || port , (err) => {
-app.listen(port, () => {
-  console.log('Server started running on : ' + port)
-})
-=======
 const app = express()
 const config = require('./config/db.config.js')
 
@@ -74,7 +33,7 @@ app.get('/DB_Request/:dynamic', function(req, res) {
   //Requerimos el modulo para sql
   var mysql = require('mysql2');
 
-  //Parseamos los contenidos de la url
+  // Parseamos los contenidos de la url
   query = req.url.replace('/DB_Request/', '').replaceAll('_', ' ')
   console.log(query)
 
@@ -98,4 +57,3 @@ app.listen(port, () => {
   console.log('Server started running on : ' + port)
 })
 
->>>>>>> feature/html_requests
